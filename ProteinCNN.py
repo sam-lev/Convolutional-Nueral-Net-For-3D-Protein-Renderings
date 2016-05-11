9c1a30616edf80244571530e71b8eedd2994d81b
sam-lev <samlev@cs.utah.edu>
2016-05-11 17:51:50 -0600

    first commit

---
+++ ProteinCNN.py
@@ -0,0 +1,356 @@
+#!/usr/bin/python
+
+import theano
+from theano import tensor as T
+from theano.tensor.nnet import conv
+from theano.tensor.signal import downsample
+
+import numpy
+import pylab
+from PIL import Image
+
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+class HiddenLayer(object):
+    def __init__(self, rng, input, n_in, n_out, W=None, b=None,
+                 activation=T.tanh):
+        """
+        Typical hidden layer of a MLP: units are fully-connected and have
+        sigmoidal activation function. Weight matrix W is of shape (n_in,n_out)
+        and the bias vector b is of shape (n_out,).
+
+        NOTE : The nonlinearity used here is tanh
+
+        Hidden unit activation is given by: tanh(dot(input,W) + b)
+
+        :type rng: numpy.random.RandomState
+        :param rng: a random number generator used to initialize weights
+
+        :type input: theano.tensor.dmatrix
+        :param input: a symbolic tensor of shape (n_examples, n_in)
+
+        :type n_in: int
+        :param n_in: dimensionality of input
+
+        :type n_out: int
+        :param n_out: number of hidden units
+
+        :type activation: theano.Op or function
+        :param activation: Non linearity to be applied in the hidden
+                           layer
+        """
+        self.input = input
+
+
+        # `W` is initialized with `W_values` which is uniformely sampled
+        # from sqrt(-6./(n_in+n_hidden)) and sqrt(6./(n_in+n_hidden))
+        # for tanh activation function
+        # the output of uniform if converted using asarray to dtype
+        # theano.config.floatX so that the code is runable on GPU
+        # Note : optimal initialization of weights is dependent on the
+        #        activation function used (among other things).
+        #        For example, results presented in [Xavier10] suggest that you
+        #        should use 4 times larger initial weights for sigmoid
+        #        compared to tanh
+        #        We have no info for other function, so we use the same as
+        #        tanh.
+        if W is None:
+            W_values = numpy.asarray(
+                rng.uniform(
+                    low=-numpy.sqrt(6. / (n_in + n_out)),
+                    high=numpy.sqrt(6. / (n_in + n_out)),
+                    size=(n_in, n_out)
+                ),
+                dtype=theano.config.floatX
+            )
+            if activation == theano.tensor.nnet.sigmoid:
+                W_values *= 4
+
+            W = theano.shared(value=W_values, name='W', borrow=True)
+
+        if b is None:
+            b_values = numpy.zeros((n_out,), dtype=theano.config.floatX)
+            b = theano.shared(value=b_values, name='b', borrow=True)
+
+        self.W = W
+        self.b = b
+
+        lin_output = T.dot(input, self.W) + self.b
+        self.output = (
+            lin_output if activation is None
+            else activation(lin_output)
+        )
+
+class LogisticRegression(object):
+    """Multi-class Logistic Regression Class
+
+    The logistic regression is fully described by a weight matrix :math:`W`
+    and bias vector :math:`b`. Classification is done by projecting data
+    points onto a set of hyperplanes, the distance to which is used to
+    determine a class membership probability.
+    """
+
+    def __init__(self, input, n_in, n_out):
+        """ Initialize the parameters of the logistic regression
+
+        :type input: theano.tensor.TensorType
+        :param input: symbolic variable that describes the input of the
+                      architecture (one minibatch)
+
+        :type n_in: int
+        :param n_in: number of input units, the dimension of the space in
+                     which the datapoints lie
+
+        :type n_out: int
+        :param n_out: number of output units, the dimension of the space in
+                      which the labels lie
+
+        """
+        # start-snippet-1
+        # initialize with 0 the weights W as a matrix of shape (n_in, n_out)
+        self.W = theano.shared(
+            value=numpy.zeros(
+                (n_in, n_out),
+                dtype=theano.config.floatX
+            ),
+            name='W',
+            borrow=True
+        )
+        # initialize the biases b as a vector of n_out 0s
+        self.b = theano.shared(
+            value=numpy.zeros(
+                (n_out,),
+                dtype=theano.config.floatX
+            ),
+            name='b',
+            borrow=True
+        )
+
+        # symbolic expression for computing the matrix of class-membership
+        # probabilities
+        # Where:
+        # W is a matrix where column-k represent the separation hyperplane for
+        # class-k
+        # x is a matrix where row-j  represents input training sample-j
+        # b is a vector where element-k represent the free parameter of
+        # hyperplane-k
+        self.p_y_given_x = T.nnet.softmax(T.dot(input, self.W) + self.b)
+
+        # symbolic description of how to compute prediction as class whose
+        # probability is maximal
+        self.y_pred = T.argmax(self.p_y_given_x, axis=1)
+        # end-snippet-1
+
+        # parameters of the model
+        self.params = [self.W, self.b]
+
+        # keep track of model input
+        self.input = input
+
+class LeNetConvPoolLayer(object):
+    """Pool Layer of a convolutional network """
+
+    def __init__(self, rng, input, filter_shape, image_shape, poolsize=(2, 2)):
+        """
+        Allocate a LeNetConvPoolLayer with shared variable internal parameters.
+
+        :type rng: numpy.random.RandomState
+        :param rng: a random number generator used to initialize weights
+
+        :type input: theano.tensor.dtensor4
+        :param input: symbolic image tensor, of shape image_shape
+
+        :type filter_shape: tuple or list of length 4
+        :param filter_shape: (number of filters, num input feature maps,
+                              filter height, filter width)
+
+        :type image_shape: tuple or list of length 4
+        :param image_shape: (batch size, num input feature maps,
+                             image height, image width)
+
+        :type poolsize: tuple or list of length 2
+        :param poolsize: the downsampling (pooling) factor (#rows, #cols)
+        """
+
+        assert image_shape[1] == filter_shape[1]
+        self.input = input
+
+        # there are "num input feature maps * filter height * filter width"
+        # inputs to each hidden unit
+        fan_in = numpy.prod(filter_shape[1:])
+        # each unit in the lower layer receives a gradient from:
+        # "num output feature maps * filter height * filter width" /
+        #   pooling size
+        fan_out = (filter_shape[0] * numpy.prod(filter_shape[2:]) /
+                   numpy.prod(poolsize))
+        # initialize weights with random weights
+        W_bound = numpy.sqrt(6. / (fan_in + fan_out))
+        self.W = theano.shared(
+            numpy.asarray(
+                rng.uniform(low=-W_bound, high=W_bound, size=filter_shape),
+                dtype=theano.config.floatX
+            ),
+            borrow=True
+        )
+
+        # the bias is a 1D tensor -- one bias per output feature map
+        b_values = numpy.zeros((filter_shape[0],), dtype=theano.config.floatX)
+        self.b = theano.shared(value=b_values, borrow=True)
+
+        # convolve input feature maps with filters
+        conv_out = conv.conv2d(
+            input=input,
+            filters=self.W,
+            filter_shape=filter_shape,
+            image_shape=image_shape
+        )
+
+        # downsample each feature map individually, using maxpooling
+        pooled_out = downsample.max_pool_2d(
+            input=conv_out,
+            ds=poolsize,
+            ignore_border=True
+        )
+
+        # add the bias term. Since the bias is a vector (1D array), we first
+        # reshape it to a tensor of shape (1, n_filters, 1, 1). Each bias will
+        # thus be broadcasted across mini-batches and feature map
+        # width & height
+        self.output = T.nnet.sigmoid(pooled_out + self.b.dimshuffle('x', 0, 'x', 'x'))
+
+        # store parameters of this layer
+        self.params = [self.W, self.b]
+
+        # keep track of model input
+        self.input = input
+
+rng = numpy.random.RandomState(23455)
+'''
+# instantiate 4D tensor for input
+input = T.tensor4(name='input')
+
+# initialize shared variable for weights.
+w_shp = (2, 3, 9, 9)
+w_bound = numpy.sqrt(3 * 9 * 9)
+W = theano.shared( numpy.asarray(
+            rng.uniform(
+                low=-1.0 / w_bound,
+                high=1.0 / w_bound,
+                size=w_shp),
+            dtype=input.dtype), name ='W')
+
+# initialize shared variable for bias (1D tensor) with random values
+# IMPORTANT: biases are usually initialized to zero. However in this
+# particular application, we simply apply the convolutional layer to
+# an image without learning the parameters. We therefore initialize
+# them to random values to "simulate" learning.
+b_shp = (2,)
+b = theano.shared(numpy.asarray(
+            rng.uniform(low=-.5, high=.5, size=b_shp),
+            dtype=input.dtype), name ='b')
+
+# build symbolic expression that computes the convolution of input with filters in w
+conv_out = conv.conv2d(input, W)
+
+'''
+
+# build symbolic expression to add bias and apply activation function, i.e. produce neural net layer output
+# A few words on ``dimshuffle`` :
+#   ``dimshuffle`` is a powerful tool in reshaping a tensor;
+#   what it allows you to do is to shuffle dimension around
+#   but also to insert new ones along which the tensor will be
+#   broadcastable;
+#   dimshuffle('x', 2, 'x', 0, 1)
+#   This will work on 3d tensors with no broadcastable
+#   dimensions. The first dimension will be broadcastable,
+#   then we will have the third dimension of the input tensor as
+#   the second of the resulting tensor, etc. If the tensor has
+#   shape (20, 30, 40), the resulting tensor will have dimensions
+#   (1, 40, 1, 20, 30). (AxBxC tensor is mapped to 1xCx1xAxB tensor)
+#   More examples:
+#    dimshuffle('x') -> make a 0d (scalar) into a 1d vector
+#    dimshuffle(0, 1) -> identity
+#    dimshuffle(1, 0) -> inverts the first and second dimensions
+#    dimshuffle('x', 0) -> make a row out of a 1d vector (N to 1xN)
+#    dimshuffle(0, 'x') -> make a column out of a 1d vector (N to Nx1)
+#    dimshuffle(2, 0, 1) -> AxBxC to CxAxB
+#    dimshuffle(0, 'x', 1) -> AxB to Ax1xB
+#    dimshuffle(1, 'x', 0) -> AxB to Bx1xA
+#output = T.nnet.sigmoid(conv_out + b.dimshuffle('x', 0, 'x', 'x'))
+
+# create theano function to compute filtered images
+#f = theano.function([input], output)
+# instantiate 4D tensor for input
+input = T.tensor4(name='input')
+
+# open random image of dimensions 507x508 (Height x Width)
+img = Image.open(open('./imageRight.jpg'))
+# dimensions are (height, width, channel)
+img = numpy.asarray(img, dtype='float64') / 256.
+
+layer_input = img.transpose(2, 0, 1).reshape(1, 3, 507, 508)
+
+print(layer_input.shape)
+# Construct the first convolutional pooling layer:
+# filtering reduces the image size to (28-5+1 , 28-5+1) = (24, 24)
+# maxpooling reduces this further to (24/2, 24/2) = (12, 12)
+# 4D output tensor is thus of shape (batch_size, nkerns[0], 12, 12)
+layer0 = LeNetConvPoolLayer(
+    rng,
+    input=T.tensor4(name = 'input'),
+    image_shape=(1, 3, 507, 508),
+    filter_shape=(1, 3, 27, 28),
+    poolsize=(2, 2)
+)
+
+f = theano.function([input],layer0.output)
+layer0_ConvPool = layer0.output
+# the HiddenLayer being fully-connected, it operates on 2D matrices of
+# shape (batch_size, num_pixels) (i.e matrix of rasterized images).
+# This will generate a matrix of shape (batch_size, nkerns[1] * 4 * 4),
+# or (500, 50 * 4 * 4) = (500, 800) with the default values.
+
+
+layer1_input = layer0.output.flatten(2)
+
+
+# construct a fully-connected sigmoidal layer
+layer1 = HiddenLayer(
+    rng,
+    input=layer1_input,
+    n_in=1 * 4 * 4,
+    n_out=500,
+    activation=T.tanh
+)
+
+#layer1_pool =  LogisticRegression(input = layer1.output, n_in=500, n_out=10)
+
+# put image in 4D tensor of shape (1, 3, height, width)
+
+f_Conv1Pool1 = theano.function([input],  layer0.output)
+
+
+f = f_Conv1Pool1(layer_input)
+#first element nkerns[0] ?
+
+# ___one convolution and one output tensor values____
+#filtered_img = f(layer_input)
+
+#first convolution
+#print(filtered_img[0,1,:,:].shape)
+#second convolution
+#print(filtered_img[0,0,:,:])
+'''
+# plot original image and first and second components of output
+pylab.subplot(1, 3, 1); pylab.axis('off'); pylab.imshow(img)
+#pylab.gray();
+# recall that the convOp output (filtered image) is actually a "minibatch",
+# of size 1 here, so we take index 0 in the first dimension:
+pylab.subplot(1, 3, 2); pylab.axis('off'); pylab.imshow(filtered_img[0, 0, :, :])
+pylab.subplot(1, 3, 3); pylab.axis('off'); pylab.imshow(filtered_img[0, 1, :, :])
+pylab.show()
+
+'''
+
